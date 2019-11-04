6439cedda7835dea86f3e065a3a0bbd5eaea872b
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-04 22:44:29 +1100

    feat: Implement required data retrieval for home page

--- home/web_app.py
+++ home/web_app.py
@@ -1,7 +1,13 @@
 from flask.views import MethodView
+from app import session
+from category.models import Category
+from catalog_item.models import CatalogItem
 
 
 class ShowHomePage(MethodView):
 
     def dispatch_request(self, *args, **kwargs):
+        latest_items = 2
+        categories = session.query(Category).all()
+        latest_catalog_items = session.query(CatalogItem).limit(latest_items).all()
         return "Hello World!!"
