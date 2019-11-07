1410c3219cf54eb8b72e4f17f7e2b5ee55a8e5fc
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-07 20:26:35 +1100

    feat: Implement basic Category item listing

--- category/views.py
+++ category/views.py
@@ -1,5 +1,16 @@
 from flask import Blueprint
-from category.web_app import CreateNewCategory
+from category.web_app import GetCategoryItems
 
 
 category_app = Blueprint("category_app", __name__)
+
+# web app
+get_category_items_web_view = GetCategoryItems.as_view(
+    "get_category_items_web_view"
+)
+
+category_app.add_url_rule(
+    "/catalog/<category_name>/items",
+    view_func=get_category_items_web_view,
+    methods=["GET", ]
+)
--- category/web_app.py
+++ category/web_app.py
@@ -1,6 +1,22 @@
 from flask.views import MethodView
+from flask import render_template
+from app import session
 from category.models import Category
+from catalog_item.models import CatalogItem
 
 
-class CreateNewCategory(MethodView):
-    pass
+class GetCategoryItems(MethodView):
+
+    def dispatch_request(self, category_name):
+        categories = session.query(Category).all()
+        category = session.query(Category).filter_by(name=category_name).one()
+        catalog_items = session.query(CatalogItem).filter_by(
+            category_id=category.id
+        ).all()
+        return render_template(
+            "category_items.html",
+            heading=category_name,
+            categories=categories,
+            category=category,
+            category_items=catalog_items
+        )
