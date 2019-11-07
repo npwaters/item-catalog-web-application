54f7540057d99668e0e38f3623361603d2d66c5d
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-07 21:05:18 +1100

    refactor: Move category related template to blueprint template directory

--- category/views.py
+++ category/views.py
@@ -2,7 +2,11 @@ from flask import Blueprint
 from category.web_app import GetCategoryItems
 
 
-category_app = Blueprint("category_app", __name__)
+category_app = Blueprint(
+    "category_app",
+    __name__,
+    template_folder="blueprint_templates/category"
+)
 
 # web app
 get_category_items_web_view = GetCategoryItems.as_view(
