03f1f79d7c59664f10a5b19ebe2f1bd53d66ad4b
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-12 19:32:56 +1100

    refactor: Switch to get catalog item helper in get catalog item view function

--- catalog_item/web_app.py
+++ catalog_item/web_app.py
@@ -1,21 +1,16 @@
 from flask.views import MethodView
 from flask import render_template
 from app import session, login_session
-from catalog_item.models import CatalogItem
-from category.models import Category
+from helpers.catalog_item_helpers import get_catalog_item
 
 
 class GetCatalogItem(MethodView):
 
     def dispatch_request(self, category_name, catalog_item_name):
-        category = session.query(Category).filter_by(
-            name=category_name
-        ).one()
-
-        catalog_item = session.query(CatalogItem).filter_by(
-            name=catalog_item_name,
-            category_id=category.id
-        ).one()
+        catalog_item = get_catalog_item(
+            category_name,
+            catalog_item_name
+        )
         return render_template(
             "read_catalog_item.html",
             catalog_item=catalog_item,
