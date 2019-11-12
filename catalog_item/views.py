f7a60f9a8a796633e50ace14e3ad9061d36deaa3
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-12 20:45:54 +1100

    feat: Implement basic Update catalog item functionality
    Redirect the user back to the category items list.

--- catalog_item/views.py
+++ catalog_item/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from catalog_item.web_app import GetCatalogItem
+from catalog_item.web_app import GetCatalogItem, EditCatalogItem
 
 
 catalog_item_app = Blueprint(
@@ -12,9 +12,18 @@ catalog_item_app = Blueprint(
 get_catalog_item_web_view = GetCatalogItem.as_view(
     "get_catalog_item_web_view"
 )
+edit_catalog_item_web_view = EditCatalogItem.as_view(
+    "edit_catalog_item_web_view"
+)
 
 catalog_item_app.add_url_rule(
     "/catalog/<category_name>/items/<catalog_item_name>",
     view_func=get_catalog_item_web_view,
     methods=["GET", ]
 )
+catalog_item_app.add_url_rule(
+    "/catalog/<category_name>/<catalog_item_name>/edit",
+    view_func=edit_catalog_item_web_view,
+    methods=["GET", "POST", ]
+)
+
--- catalog_item/web_app.py
+++ catalog_item/web_app.py
@@ -1,7 +1,8 @@
 from flask.views import MethodView
-from flask import render_template
+from flask import render_template, jsonify, request, redirect, url_for
 from app import session, login_session
 from helpers.catalog_item_helpers import get_catalog_item
+from helpers.category_helpers import get_categories, get_category
 
 
 class GetCatalogItem(MethodView):
@@ -11,9 +12,44 @@ class GetCatalogItem(MethodView):
             category_name,
             catalog_item_name
         )
+        category = get_category(category_name)
         return render_template(
             "read_catalog_item.html",
             catalog_item=catalog_item,
+            category=category,
             STATE=login_session.get("state"),
             user=login_session.get("username")
         )
+
+
+class EditCatalogItem(MethodView):
+
+    def dispatch_request(self, category_name, catalog_item_name):
+        catalog_item = get_catalog_item(
+            category_name,
+            catalog_item_name
+        )
+        if not catalog_item:
+            # TODO: render a 404 error page
+            return jsonify({}), 404
+        if request.method == "GET":
+            categories = get_categories()
+            return render_template(
+                "edit_catalog_item.html",
+                catalog_item=catalog_item,
+                category_name=category_name,
+                categories=categories,
+            )
+        else:
+            catalog_item_form = request.form
+            if catalog_item_form.get("name"):
+                catalog_item.name = catalog_item_form.get("name")
+            session.add(catalog_item)
+            session.commit()
+            return redirect(url_for(
+                "category_app.get_category_items_web_view",
+                category_name=category_name
+                )
+            )
+
+
