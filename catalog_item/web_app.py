f3d2c1a76cc2f2163cc83285080d94e3467a7dc3
npwaters <40352015+npwaters@users.noreply.github.com>
2019-11-12 19:21:32 +1100

    feat: Add Category ID to Catalog Item query filter
    We need to make sure that we have the right catalog item in case of
    items with the same name in different categories.

--- catalog_item/web_app.py
+++ catalog_item/web_app.py
@@ -2,13 +2,19 @@ from flask.views import MethodView
 from flask import render_template
 from app import session, login_session
 from catalog_item.models import CatalogItem
+from category.models import Category
 
 
 class GetCatalogItem(MethodView):
 
     def dispatch_request(self, category_name, catalog_item_name):
+        category = session.query(Category).filter_by(
+            name=category_name
+        ).one()
+
         catalog_item = session.query(CatalogItem).filter_by(
-            name=catalog_item_name
+            name=catalog_item_name,
+            category_id=category.id
         ).one()
         return render_template(
             "read_catalog_item.html",
